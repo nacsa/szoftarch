1b989d87410e61839071f78c1e8e72417fc5e7af
nacsa <Gabor@Pupusas>
2014-11-20 16:53:23 +0100

    jaaaaaa

---
+++ vaadintest/src/main/java/com/test/vaadintest/MyVaadinUI.java
@@ -0,0 +1,51 @@
+package com.test.vaadintest;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.tapio.googlemaps.GoogleMap;
+import com.vaadin.tapio.googlemaps.client.LatLon;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI
+{
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.test.vaadintest.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+
+        GoogleMap googleMap = new GoogleMap(null, null, null);
+        googleMap.setCenter(new LatLon(60.440963, 22.25122));
+        googleMap.setZoom(10);
+        googleMap.setSizeFull();
+        layout.addComponent(googleMap);
+        layout.setExpandRatio(googleMap, 1.0f);
+        
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+        
+        
+    }
+
+}
---
+++ vaadintest/src/main/webapp/VAADIN/widgetsets/com.test.vaadintest.AppWidgetSet/deferredjs/C19D07445D41EB97F767174ABCB95B48/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_test_vaadintest_AppWidgetSet.runAsyncCallback2('zbb(865,1,fwd);_.vc=function x8b(){YYb((!RYb&&(RYb=new bZb),RYb),this.b.e)};Hqd(pi)(2);\n//# sourceURL=com.test.vaadintest.AppWidgetSet-2.js\n')
